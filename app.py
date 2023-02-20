424e74e5d9495a02fdfc52d4e0174684f5d17a55
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-20 15:38:31 -0300

    stuff

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+from distutils import debug
 from scripts.scraping import run_spider
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
@@ -19,5 +24,5 @@ def index():
     return render_template('index.html', results=results)
 
 if __name__ == '__main__':
+    app.run(debug=True)
     run_spider()
-    app.run()
