f0bc83e9393e7d3ce8ebbec6d03153db250550da
robsoncardozo <robsonleaocardozo@hotmail.com>
2023-02-21 18:18:54 -0300

    json treat updated & results.html

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 import requests
-from flask import Flask, render_template, send_from_directory, redirect, url_for, request
-from pymongo import MongoClient
 import json
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+    redirect,
+    url_for,
+    request,
+)
+from pymongo import MongoClient
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
@@ -32,67 +39,74 @@ class WikipediaSpider(scrapy.Spider):
                 "titles": title,
                 "exsentences": 2,
                 "explaintext": True,
-                "inprop": "url"
-            }
+                "inprop": "url",
+            },
         ).json()
 
         page = next(iter(api_response["query"]["pages"].values()))
         url = page["fullurl"]
 
-        record = {
-            'title': title,
-            'summary': summary,
-            'url': url,
-            'query': self.query
-        }
+        record = {"title": title, "summary": summary, "url": url, "query": self.query}
         self.records.append(record)
 
     def closed(self, reason):
-        filename = f"{self.query}.json"
-        with open(os.path.join("data", filename), "w") as file:
-            json.dump(self.records, file)
+        # Salva os resultados em um arquivo JSON
+        with open("data/{}.json".format(self.query), "w") as f:
+            json.dump(self.records, f)
 
 
 app = Flask(__name__)
 
 # Flask app routes
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', results=[])
+    return render_template("index.html")
 
 
-@app.route('/search')
+@app.route("/search")
 def search():
-    query = request.args.get('query')
+    query = request.args.get("query")
     if not query:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
-    spider = WikipediaSpider(query=query)
+    # Executa o spider para buscar os dados
     process = CrawlerProcess(get_project_settings())
+    spider = WikipediaSpider(query=query)
     process.crawl(spider)
     process.start()
 
-    # Retrieve the search results from the file saved by the spider
-    filename = f"{query}.json"
-    with open(os.path.join("data", filename), "r") as file:
-        search_results = json.load(file)
+    # Redireciona para a página de resultados
+    return redirect(url_for("show_results", query=query))
 
+
+@app.route("/results/<query>")
+def show_results(query):
+    # Carrega os dados do arquivo JSON
+    file_path = f"data/{query}.json"
+    with open(file_path, "r", encoding="utf-8") as f:
+        search_results = json.load(f)
+
+    # Formata os resultados para exibição na página
     formatted_results = []
     for result in search_results:
         formatted_result = {
-            'title': result['title'],
-            'summary': result['summary'],
-            'url': result['url']
+            "title": result["title"],
+            "summary": result["summary"],
+            "url": result["url"],
         }
         formatted_results.append(formatted_result)
 
-    return render_template('index.html', results=formatted_results)
+    return render_template("results.html", query=query, results=formatted_results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
